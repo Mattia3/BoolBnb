02d76b693d6bf4629e2975cccb6d6667e08050a9
ricardo-limaco <ricardolimaco.05@gmail.com>
2022-02-14 15:48:31 +0100

    create navbar

---
+++ resources/views/host/apartments/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.host')
+@section("page_title", "Dashboard")
+
+@section("bg-hero", "bg-hero-dashboard")
+
+
+
+
+@section('content')
+@endsection
---
+++ resources/views/host/partials/the_header.blade.php
@@ -0,0 +1,33 @@
+
+<div class="bg-hero @yield('bg-hero') position-relative">
+  <div class="d-flex justify-content-between px-4 py-4">
+    <a href="">About us</a>
+    <a href=""><h1>Boolbnb</h1></a>
+    <a href="">Login</a>
+  </div>
+
+
+  <div class="d-flex position-absolute">
+
+    <div class="">
+      <img src="" alt="profilo">
+    </div>
+    
+    <div>
+      <h3>Nome utente</h3>
+      <ul>
+        <li><a href="">Profilo utente</a></li>
+        <li><a href="">Gestisci annunci</a></li>
+        <li><a href="">Messaggi</a></li>
+        <li><a href="">Pubblica annuncio</a></li>
+        <li><a href="">Statistiche</a></li>
+      </ul>
+    </div>
+  </div>
+
+</div>
+
+
+  
+
+
