78156d5f978270e232e01f6f6176940d8e2f8a60
Mattia <mcortinovi9@gmail.com>
2022-02-17 12:44:41 +0100

    Create sponsor

---
+++ resources/views/host/apartments/sponsor.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.host')
+@section("page_title", "Sponsor")
+@section('hero') bg-index @endsection
+@section('content')
+    
+  <div class="container">
+    <div class="my-5">
+      <h2>METTI IN EVIDENZA LA TUA STRUTTURA</h2>
+    </div>
+  </div>
+@endsection
