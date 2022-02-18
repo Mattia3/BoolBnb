6f91bf03ec01def137b36c4a6e665b15276d11dc
mariaacanfora <ma.maria.acanfora@gmail.com>
2022-02-18 10:59:18 +0100

    Changed navbar in filter page with link on login + added route in web.php to page's filter linked to HomeController

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Route::get('/', function () {
  
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/search', 'HomeController@index')->name('home');
 
 Route::middleware('auth')
     ->namespace('Host') //riaggiungere middleware('auth') 
