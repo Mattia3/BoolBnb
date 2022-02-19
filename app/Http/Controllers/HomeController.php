e22caa3c3f61789adba9caf459ca5ff6e87698c8
mariaacanfora <ma.maria.acanfora@gmail.com>
2022-02-19 11:24:10 +0100

    Push before pull from Ale

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Apartment;
+use App\Service;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -22,8 +24,12 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index()
-    {
-        // return view('home');
-        return view('guest.filter');
+    {   
+        $services = Service::all();
+        $apartments = Apartment::with(['services','rules', 'sponsors'])->get();
+        return view('guest.filter', [
+            'services' => $services,
+            'apartments' => $apartments
+        ]);
     }
 }
