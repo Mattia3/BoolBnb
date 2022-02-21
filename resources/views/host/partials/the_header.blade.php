f0045148be25ef3128bcc1c10676588da37a51e4
ricardo-limaco <ricardolimaco.05@gmail.com>
2022-02-21 10:39:42 +0100

    Navbar responsive

--- resources/views/host/partials/the_header.blade.php
+++ resources/views/host/partials/the_header.blade.php
@@ -6,7 +6,7 @@
     <ul class="navbar-nav ml-auto">
       <!-- Authentication Links -->
       <h5>
-          <div class="dropdown">
+        <div class="dropdown">
           <a class="dropdown-toggle text-white" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
             {{ Auth::user()->name }}
           </a>
@@ -25,30 +25,51 @@
       </h5>
   </ul>
   </div>
-</div>
 
 
-<div class="container">
-  <div class="row mt-50 align-items-center">
 
-    {{-- Image Profile --}}
-    {{-- <div class="col-lg-2 col-md-2 col-sm-3 col-xs-12"> --}}
-    <div class="img-profile">
-      {{-- <div class="img-profile"></div> --}}
-      <img src="{{ asset('storage/host/host-1.jpg') }}" alt="profile">
+  <div class="container _p-nav">
+    <div class="row align-items-center">
+  
+      {{-- Image Profile --}}
+      <img class="img-profile" src="{{ asset('storage/host/host-2.jpg') }}" alt="profile">
+  
+  
+      {{-- Link --}}
+      <div class="col-lg-10 col-sm-9 _d-none-sm">
+        <h3 class="text-white mb-3"> {{ Auth::user()->name }} {{ Auth::user()->last_name }} </h3>
+        <ul class="d-flex flex-wrap navbar-link">
+          <li><a href="{{route('host.dashboard')}}">Profilo utente</a></li>
+          <li><a href="{{route('host.apartments.index')}}">Gestisci annunci</a></li>
+          <li><a href="">Messaggi</a></li>
+          <li><a href="{{route('host.apartments.create')}}">Pubblica annuncio</a></li>
+          <li><a href="">Statistiche</a></li>
+        </ul>
+      </div>
+
+
+      {{-- Hamburger --}}
+      <div class="col-6 _d-none-hamburger"> 
+        <h3 class="text-white mb-3"> {{ Auth::user()->name }} {{ Auth::user()->last_name }} </h3>
+
+        <div class="dropdown">
+          <button class="btn-hamburger dropdown-toggle" type="button" id="dropdownMenu2" data-bs-toggle="dropdown" aria-expanded="false">
+            <i class="fa-solid fa-bars text-white"></i>
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
+            <li><a class="dropdown-item" href="{{route('host.dashboard')}}">Profilo utente</a></li>
+            <li><a class="dropdown-item" href="{{route('host.apartments.index')}}">Gestisci annunci</a></li>
+            <li><a class="dropdown-item" href="">Messaggi</a></li>
+            <li><hr class="m-1"></li>
+            <li><a class="dropdown-item" href="{{route('host.apartments.create')}}">Pubblica annuncio</a></li>
+            <li><a class="dropdown-item" href="">Statistiche</a></li>
+          </ul>
+        </div>
+      </div>
+
     </div>
-
-    {{-- Link --}}
-    <div class="col-lg-10 col-md-8 col-sm-7 col-xs-12">
-      <h3 class="text-white mb-3"> {{ Auth::user()->name }} {{ Auth::user()->last_name }} </h3>
-      <ul class="d-flex flex-wrap navbar-link">
-        <li><a href="{{route('host.dashboard')}}">Profilo utente</a></li>
-        <li><a href="{{route('host.apartments.index')}}">Gestisci annunci</a></li>
-        <li><a href="">Messaggi</a></li>
-        <li><a href="{{route('host.apartments.create')}}">Pubblica annuncio</a></li>
-        <li><a href="">Statistiche</a></li>
-      </ul>
-    </div>
-
   </div>
+
 </div>
+
+
