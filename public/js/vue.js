9a68835f0e372a7b7797507e9f89701ed758379e
ddecataldo <davidedecataldo92@gmail.com>
2022-02-25 17:44:41 +0100

    push before from Ale

--- public/js/vue.js
+++ public/js/vue.js
@@ -14576,7 +14576,7 @@ var app = new Vue({
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Utente\Desktop\Boolean_#43\BoolBnb\resources\js\vue.js */"./resources/js/vue.js");
+module.exports = __webpack_require__(/*! D:\Boolean\BoolBnb\resources\js\vue.js */"./resources/js/vue.js");
 
 
 /***/ })
