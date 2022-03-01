eb9cb292becc7bc32c681be1f45a7123020fde16
Alessandro-Fedele <al.fedele@outlook.it>
2022-03-01 10:45:34 +0100

    push for Mattia

--- resources/views/host/dashboard.blade.php
+++ resources/views/host/dashboard.blade.php
@@ -60,29 +60,29 @@
         </div>
     </div>
     <div class="row row-cols-1 row-cols-lg-4 row-cols-md-3 row-cols-sm-2">
-      @if(count($highlitedApartments) != 0)
+        @if(count($highlitedApartments) != 0)
         @foreach ($highlitedApartments as $apartment)
         <div class="col mb-4">
-          <div class="card">
-            <div class="position-relative">
-    
-              <img src="{{ asset('storage/' . $apartment->cover_img) }}" class="card-img-top" alt="{{ $apartment->title }} ">
-    
-              <div class="number-card-price">
-                <i class="fa-solid fa-euro-sign"></i>
-                <strong>{{ $apartment->price }}</strong>
-              </div>
+            <div class="card">
+                <div class="position-relative">
+
+                    <img src="{{ asset('storage/' . $apartment->cover_img) }}" class="card-img-top" alt="{{ $apartment->title }} ">
+
+                    <div class="number-card-price">
+                        <i class="fa-solid fa-euro-sign"></i>
+                        <strong>{{ $apartment->price }}</strong>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <a href="{{route('host.apartments.show', $apartment->slug)}}" class="card-title"><strong> {{ Str::limit($apartment->title, 35) }} </strong> </a>
+                    <p class="card-text">{{ Str::limit($apartment->description, 200) }}</p>
+                </div>
             </div>
-            <div class="card-body">
-              <a href="{{route('host.apartments.show', $apartment->slug)}}" class="card-title"><strong> {{ Str::limit($apartment->title, 35) }} </strong> </a>
-              <p class="card-text">{{ Str::limit($apartment->description, 200) }}</p>
-            </div>
-          </div>
         </div>
         @endforeach
-      @else
+        @else
         <span>Non hai appartamenti sponsorizzati...</span>
-      @endif
+        @endif
     </div>
 </div>
 
@@ -90,6 +90,9 @@
     <div class="row pb-5">
         <div class="col">
             <h2>Statistiche Generali</h2>
+            <div>
+                <canvas id="myChart"></canvas>
+            </div>
         </div>
     </div>
 </div>
